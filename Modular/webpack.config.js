8eedf7d7efb233109b798e20f3b4d543ebe91bbd
jsantis <san21153@uvg.edu.gt>
2023-03-28 21:17:09 -0600

    webpack config

---
+++ Modular/webpack.config.js
@@ -0,0 +1,35 @@
+const path = requiere('path');
+const HtmlWebpackPlugin = requiere('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        Principal: './src/Principal/index.js',
+        NLanzamientos: './src/NuevosLanzamientos/nuevos-lanzamientos.js',
+        Populares: './src/Populares/populares.js',
+        Historia1: './src/Historia1/historia1.js',
+        HistoriaAventura: './src/Historia2.1/HistoriaAventura.js',
+        HistoriaDolor: './src/Historia2.2/HistoriaDolor.js',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+            template: './src/index.html',
+            chunks: ['index']
+        })
+    ]
+};
