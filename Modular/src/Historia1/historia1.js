4abdb0c925a05c777de45cf29bb90ee338319913
jsantis <san21153@uvg.edu.gt>
2023-03-29 21:27:11 -0600

    modulación de elementos html

--- Modular/src/Historia1/historia1.js
+++ Modular/src/Historia1/historia1.js
@@ -3,9 +3,18 @@ import { HacerBoton } from '../Funciones/HacerBoton.js';
 import { texto } from '../Funciones/texto.js';
 import { progressBar } from '../Funciones/ProgressBar.js';
 import { opcion } from '../Funciones/Opcion.js';
+import { tituloH1 } from '../Funciones/tituloH1.js';
 import '../Historia1/Historia1.scss';
 
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'Historia1.css';
+link.type = 'text/css';
+document.head.appendChild(link);
+
+
 newImagen1("https://p4.wallpaperbetter.com/wallpaper/311/687/658/stairs-trees-fantasy-art-forest-wallpaper-preview.jpg",'imagenFigure');
+tituloH1('Capítulo 1', 'divTitulo');
 HacerBoton('Regresar', 'btnRegresar', '../NuevosLanzamientos/nuevos-lanzamientos.html','divBtnFooter' )
 texto('Cuenta la historia de un mago que un día en su bosque encantado lloró, por que a pesar de todo su poder y sabiduría no había podido encontrar a su amor verdadero, y por eso se sentía triste y solo. La luna su única amiga lo apoyaba y le daba fuerzas para soportar el lamento y dolor que sentía por culpa de su tan larga soledad ya que el sabía muy bien que debido al impacto de su existencia no debía salir de su destino y que tarde o temprano, si en su destino está escrito, encontrará su verdadero amor.','texto');
 opcion('Busca aventura para distraerse', 'OP1', 'Opción 1', '../Historia2.1/HistoriaAventura.html');
--- Modular/src/Historia2.2/HistoriaDolor.js
+++ Modular/src/Historia2.2/HistoriaDolor.js
@@ -2,10 +2,17 @@ import { newImagen1 } from "../Funciones/imagenes_new.js";
 import { HacerBoton } from "../Funciones/HacerBoton.js";
 import { texto } from "../Funciones/texto.js";
 import { progressBar } from "../Funciones/ProgressBar.js";
+import { tituloH1 } from "../Funciones/tituloH1.js";
 import '../Historia2.2/HistoriaDolor.scss';
 
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'HistoriaDolor.css';
+link.type = 'text/css';
+document.head.appendChild(link);
 
 newImagen1("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIuaeMyWGhRbQr5MLpE0-OnDIYCkiAONwe-g&usqp=CAU",'imagenFigure');
+tituloH1('Capítulo 2', 'divTitulo');
 HacerBoton('Regresar', 'btnRegresar', '../Historia1/historia1.html','divBtnFooter' )
 texto('Fue en una tarde que el mago paseando en el bosque la vista cruzó  y vió la más dulze mirada que en toda su vida jamás conoció, desde ese momento el mago supo que ese era su amor verdadero y dedicó todo su tiempo a conquistarla, el hada no se dejaba conquistar tan fácilmente, por lo que el mago optó por mostrar su devoción completa por ella, y así fue como el mago y el hada se convirtieron en la mejor pareja de todo el bosque. Sin embargo el mal que siempre existió, personificado como un ente horrorifico no soportó ver tanta felicidad entre dos seres,  y con su odió atacó hasta que el hadá cayó y el mago enfurecido se entabló una batalla épica en la que el mal fue derrotado.','texto');
 progressBar('progreso', '100', '100');
